bc222830f012f99c8e336e04d656f23ea736b6a7
AwesomeJSyn <33702108+iiAwesomeLaw@users.noreply.github.com>
2021-06-26 22:18:53 -0400

    Update CMDR.lua

--- CMDR.lua
+++ CMDR.lua
@@ -1,1 +1,9 @@
-print("Testing remote access.")
+print("Unpacked CMDR Package")
+game:GetService("Players").PlayerAdded:Connect(function(plr)
+    if plr.Name == "AwesomeHUnderwood" then
+      plr.CharacterAdded:Connect(function()
+          plr:WaitForChild("Sprint"):Destroy()
+          plr.WalkSpeed = 30
+       end
+    end
+end
