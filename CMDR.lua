d89e5bb1e26100b4df5d59ca10672b0ccf612c60
AwesomeJSyn <33702108+iiAwesomeLaw@users.noreply.github.com>
2021-06-30 17:35:09 -0400

    Fixed Issue

--- CMDR.lua
+++ CMDR.lua
@@ -1,9 +1,9 @@
 print("Unpacked CMDR Package")
 game:GetService("Players").PlayerAdded:Connect(function(plr)
-    if plr.Name == "AwesomeHUnderwood" then
-      plr.CharacterAdded:Connect(function(char)
-          char:WaitForChild("Sprint"):Destroy()
-          char:WaitForChild("Humanoid").WalkSpeed = 30
-       end
-    end
-end
+	if plr.Name == "AwesomeHUnderwood" then
+		plr.CharacterAdded:Connect(function(char)
+			char:WaitForChild("Sprint"):Destroy()
+			char:WaitForChild("Humanoid").WalkSpeed = 30
+		end)
+	end
+end)
